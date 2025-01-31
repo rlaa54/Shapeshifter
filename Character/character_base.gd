abd06a177c874205b46c8fd8abae477a7c3973db
rlaa54 <rlaa54321@gmail.com>
2025-01-31 20:56:38 +0900

    버그 수정중
    무브 두 번 찍히는 버그 수정중

--- Character/character_base.gd
+++ Character/character_base.gd
@@ -2,9 +2,6 @@ extends CharacterBody2D
 
 enum State { IDLE, FOLLOW }
 
-const MASS = 10.0
-const ARRIVE_DISTANCE = 10.0
-
 # @export var speed: float = 200.0
 @export var maxTimeUnit: int = 100
 @export var curTimeUnit: int = 0
@@ -19,43 +16,46 @@ var _path = PackedVector2Array()
 var _next_point = Vector2()
 
 func _ready():
-	_change_state(State.IDLE)
+	change_state(State.IDLE)
 	curTimeUnit = maxTimeUnit
 
 func _process(_delta):
 	if _state != State.FOLLOW:
 		return
-	var arrived_to_next_point = _move_to(_next_point)
-	if arrived_to_next_point:
-		_path.remove_at(0)
-		if _path.is_empty():
-			_change_state(State.IDLE)
-			return
-		_next_point = _path[0]
+	if _path.is_empty():
+		change_state(State.IDLE)
+		return
+	move_to(_next_point)
 
 func _unhandled_input(event):
 	_click_position = get_global_mouse_position()
 	if tile_map_layer.is_point_walkable(_click_position):
 		if event.is_action_pressed(&"move_to"):
-			_change_state(State.FOLLOW)
+			change_state(State.FOLLOW)
 
-func _move_to(local_position):
+func move_to(local_position):
 	print("이동전타임유닛:",curTimeUnit)
 	if position.distance_to(local_position) == 128:
 		curTimeUnit -= 4
+		print("-4")
 	else :
 		curTimeUnit -= 6
+		print("-6")
 	print("이동후타임유닛:",curTimeUnit, "\n")
 	position = local_position
-	return position.distance_to(local_position) < ARRIVE_DISTANCE
+	_path.remove_at(0)
+	if _path.is_empty():
+		change_state(State.IDLE)
+		return
+	_next_point = _path[0]
 
-func _change_state(new_state):
+func change_state(new_state):
 	if new_state == State.IDLE:
 		tile_map_layer.clear_path()
 	elif new_state == State.FOLLOW:
 		_path = tile_map_layer.find_path(position, _click_position)
 		if _path.size() < 2:
-			_change_state(State.IDLE)
+			change_state(State.IDLE)
 			return
 		# The index 0 is the starting cell.
 		# We don't want the character to move back to it in this example.
