68d421fcc76fd1cb155122f01782b1a0ac4c68ae
rlaa54 <rlaa54321@gmail.com>
2025-02-09 22:42:48 +0900

    Update input_component.gd
    참조 오류 수정

--- input_component.gd
+++ input_component.gd
@@ -19,8 +19,11 @@ func _unhandled_input(event):
 		if event.is_action_pressed(&"move_to"):
 			move_component.path = tile_map_layer.find_path(character.position, click_position)
 			# 시작 지점을 제거
+			# 제거하는 이유는 루프에서 이동하기 전에 한 번 더 실행되는 것을 방지하기 위함
 			move_component.path.remove_at(0)
+			# 시작 지점을 제거 하기 때문에 캐릭터가 있는 위치로 이동하려 한다면
+			# 참조 오류가 발생하기 때문에 비어있다면 바로 리턴
+			if move_component.path.is_empty():
+				return	
 			move_component.is_moving = true
-
-			# fix me
 			move_component.next_point = move_component.path[0]
