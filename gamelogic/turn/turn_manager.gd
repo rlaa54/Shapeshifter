998b3a3ff5b79bc52d629131c7142965ed8eb63c
rlaa54 <rlaa54321@gmail.com>
2025-02-21 23:08:20 +0900

    턴 매니저 수정 중

--- gamelogic/turn/turn_manager.gd
+++ gamelogic/turn/turn_manager.gd
@@ -2,15 +2,12 @@ extends Node2D
 
 class_name Turn_manager
 
-signal turn_started(current_entity)
-signal turn_ended(current_entity)
-
-enum TurnState { PLAYER_TURN, ALLY_TURN, ENEMY_TURN }
+enum TurnState { PLAYER_TURN, ALLY_TURN, ENEMY_TURN, COUNT }
 
 var turn_order = [] # 턴 순서 리스트
 var current_turn_index = 0 # 현재 턴의 주체 인덱스
 var action_queue = []
-var current_state : TurnState
+var current_state
 
 func _ready() -> void:
 	turn_order = ["Player", "Ally", "Enemy"]
@@ -21,52 +18,35 @@ func start_current_state() -> void:
 	match current_state:
 		TurnState.PLAYER_TURN:
 			print("Player's turn!")
-			start_turn()
 			# 플레이어 입력 대기 등 추가 로직
 		TurnState.ALLY_TURN:
 			print("Ally's turn!")
-			start_turn()
 		TurnState.ENEMY_TURN:
 			print("Enemy's turn!")
-			start_turn()
+	
+	end_turn()
 
 func change_state(new_state: TurnState) -> void:
 	current_state = new_state
 	start_current_state()
-
-func start_turn() -> void:
-	var current_entity = turn_order[current_turn_index]
-	print("Turn start: " + current_entity)
-	turn_started.emit(current_entity)
-
-	if current_entity == "Player":
-        # 플레이어가 행동할 수 있도록 대기
-		pass
-	else:
-        # AI 캐릭터의 행동 실행
-		process_ai_turn(current_entity)
-
+	
 func end_turn() -> void:
-	var current_entity = turn_order[current_turn_index]
-	turn_ended.emit(current_entity)
-
 	match current_state:
 		TurnState.PLAYER_TURN:
 			print("Player's turn ended!")
-			change_state(TurnState.ALLY_TURN)
 		TurnState.ALLY_TURN:
 			print("Ally's turn ended!")
-			change_state(TurnState.ENEMY_TURN)
 		TurnState.ENEMY_TURN:
 			print("Enemy's turn ended!")
-			change_state(TurnState.PLAYER_TURN)
+
+	current_state = (current_state + 1) % TurnState.COUNT
+	change_state(current_state)
+
 
 func process_queue() -> void:
 	while action_queue.size() > 0:
 		var action = action_queue.pop_front()
 		await action.call()
-    
-	end_turn()
 
 func process_ai_turn(entity_name) -> void:
 	print(entity_name + " is thinking...")
