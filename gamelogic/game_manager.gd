292aeeba1289309522c5b4f3083381bdf7a9ba42
rlaa54 <rlaa54321@gmail.com>
2025-03-21 17:48:04 +0900

    gameloop 변경
    의도에 맞게 턴 순환 시스템 구현

--- gamelogic/game_manager.gd
+++ gamelogic/game_manager.gd
@@ -1,37 +1,41 @@
 extends Node2D
 
-var action_queue = [] # 실행할 액션을 저장하는 큐 혹시 몰라서 일단 만들어봄
+var action_queue : Array[Action] = [] # 실행할 액션을 저장하는 큐 혹시 몰라서 일단 만들어봄
 var idx  = 0 # 현재 턴의 캐릭터
 var characters = [] # 화면 상의 모든 캐릭터를 포함하며 화면을 떠나서 처리하려는 모든 캐릭터
 @onready var pc = $"/root/World/Player_character" # 플레이어 캐릭터
 @onready var tml = $"/root/World/TileMapLayer" # 타일맵 레이어
 enum Direction { NW, N, NE, W, E, SW, S, SE, NONE }
 
-func _process(_delta: float) -> void:
-	# 스피드가 빠른 캐릭터는 한 턴에 여러 번 움직일 수 있으므로 매 프레임마다 처리해야 함
-	gameloop()
+# 액션큐의 액션을 실행하고 판정함
+func executeAction() -> void:
+	while (not action_queue.is_empty()):
+		# 제일 앞의 액션을 꺼내 옴
+		var action = action_queue.pop_front()
+		# 플레이어를 위한 액션루프
+		while (true):
+			# result는 ActionResult
+			# 액션을 실행하고 그 결과를 반환
+			var result = action.perform()
+			# action이 실패 했다면
+			# 몬스터는 잘못된 행동을 하지 않고 플레이어는 잘못된 인풋을 할 수도 있으므로
+			# 플레이어의 경우만 생각한다면
+			# 턴을 넘기지 않고
+			# 다시 행동을 선택하게 함
+			# 플레이어의 인풋 대기 상태로
+			# 몬스터는 플레이어의 행동을 보고 반응함으로 본질적으로 이 경우에는 액션큐는 비어있어야 함
+			if not result.succeeded:
+				return
+			# alternate는 예를 들어 캐릭터가 문 앞에서 이동 할 경우 문을 여는 행위
+			# 의도에 맞게 자동으로 행위를 바꾼다
+			if result.alternative == null:
+				break
+			# alternate가 있다면 다음 action으로 바꿔줌
+			action = result.alternative
+		# 플레이어의 액션이 끝남
+		# 다음 액션으로 넘어가기 위해
+		# TODO:몬스터들이 행동하기 위해 AI를 계획해야 함
 
-func gameloop() -> void:
-	# 현재 캐릭터의 action을 가져옴
-	var action = characters[idx].get_action()
-	
-	# 현재 캐릭터가 계획한 action이 없다면 다음 캐릭터로 넘어가지 않음
-	if not action:
-		return
-
-	while (true):
-		# action을 실행
-		# result는 ActionResult를 반환받음
-		var result = action.perform()
-		# action이 실패했다면 다음 캐릭터로 넘어가지 않음
-		if not result.succeeded: 
-			return
-		# alternate는 예를 들어 캐릭터가 문 앞에서 이동 할 경우 문을 여는 행위
-		# 의도에 맞게 자동으로 행위를 바꾼다
-		if result.alternative == null:
-			break
-		# alternate가 있다면 다음 action으로 바꿔줌
-		action = result.alternative
-
-	# 다음 캐릭터로 넘어가기
-	idx = (idx + 1) % characters.size()
+# 액션큐에 액션을 추가함
+func addAction(action: Action) -> void:
+	action_queue.append(action)
