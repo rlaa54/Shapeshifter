c468efd180b6b30c4c108cd3a326be9b57ca8c50
rlaa54 <rlaa54321@gmail.com>
2025-04-19 20:25:21 +0900

    시야 내의 적에 대한 처리 중

--- AI/condition/enemy_in_sight.gd
+++ AI/condition/enemy_in_sight.gd
@@ -1,4 +1,10 @@
 extends ConditionLeaf
 
 func tick(actor, blackboard):
-    pass
+    # 시야 내의 적이 없을 때
+    if actor.sight.enemys.size() == 0:
+        return FAILURE
+    for enemy in actor.sight.enemys:
+        # 시야 내의 적을 블랙보드에 등록함
+        blackboard.set_value("sight_in_enemy", enemy)
+    return SUCCESS
--- character/collision/sight.gd
+++ character/collision/sight.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var character : Character_base = get_parent()
+var enemys : Array[Character_base] = []
 
 func _ready() -> void:
     # 시야의 크기를 타일 크기만큼 적용해서 설정함
@@ -12,7 +13,19 @@ func sight_update(radius : Tile_map_layer.Circle) -> void:
     # 시야를 업데이트함
     get_child(0).shape.radius = (radius + 0.5) * GameManager.tml.cell_size.x
 
+# area2d의 콜리전이 들어왔을 때
 func _on_area_entered(area : Area2D) -> void:
     if area.is_in_group("hurtbox"):
-        print("자신:", get_parent().name)
-        print("대상:",area.get_parent().name)
+        # 자신과 다른 타입의 캐릭터만 검사함
+        if area.get_parent().stats.type != character.stats.type:
+            print("자신:", get_parent().name)
+            print("대상:",area.get_parent().name)
+            enemys.append(area.get_parent())
+
+func _on_area_exited(area:Area2D) -> void:
+    if area.is_in_group("hurtbox"):
+        # 자신과 다른 타입의 캐릭터만 검사함
+        if area.get_parent().stats.type != character.stats.type:
+            print("자신:", get_parent().name)
+            print("대상:",area.get_parent().name)
+            enemys.erase(area.get_parent())
